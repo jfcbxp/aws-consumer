c455896ed0675c38180a8469f233d849023b7e7a
Joao Felipe <jfcbxp@gmail.com>
2022-06-10 14:34:57 -0300

    classe SnsMessage

---
+++ src/main/java/com/jfcbxp/consumer/model/SnsMessage.java
@@ -0,0 +1,33 @@
+package com.jfcbxp.consumer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SnsMessage {
+
+    @JsonProperty("Message")
+    private String message;
+
+    @JsonProperty("Type")
+    private String type;
+
+    @JsonProperty("TopicArn")
+    private String topicArn;
+
+    @JsonProperty("Timestamp")
+    private String timestamp;
+
+    @JsonProperty("MessageId")
+    private String messageId;
+}
